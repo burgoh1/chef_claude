8b7cd739977380688d5bc7b2c38f2fc5c33c9cd7
Hans Burgos <burgoshansstephen@gmail.com>
2026-07-07 22:33:46 -0700

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import ChefClaudeIcon from "../assets/chef-claude-icon.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={ChefClaudeIcon} alt="" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
